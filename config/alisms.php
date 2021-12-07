bc23eee9327aff4e4e2a211a520a46b4f941d94e
1452073959 <40657803+1452073959@users.noreply.github.com>
2021-12-07 09:28:42 +0800

    Delete alisms.php

--- config/alisms.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    // 填入自己的设置项
-    'AccessKeyId' => "LTAI5t9dppys8mmRvnfXd439",
-    'Secret'      => "JIn1bZnDnXwcslB7eqjJLnWzZEYppS",
-    'SignName'    => "天易",
-    'TemplateCode'=> "SMS_221140256"
-];
